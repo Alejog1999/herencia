25ca5dbd312de97d8f7ad727ee80c86d317fcb24
Alejandro Gonzalez <jose1999.369@hotmail.com>
2024-02-28 11:47:32 -0500

    test: creamos dos test, el de algoritmo encriptado y el de ignorar caracteres que nonforma parte del alfabeto

--- cesar.py
+++ cesar.py
@@ -1,8 +1,26 @@
 class Cesar:
+    alfabeto = list("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ")
     def __init__(self,clave):
         if isinstance(clave,int):
             self.clave = clave
         else:
             raise AttributeError(f"{clave} debe ser un entero")
 
-    
+    def encriptar(self,mensaje):
+        '''
+        Para cada caracter del mensaje
+        1. Obtener su posiicon en el alfabeto
+        2. Sumar clave a posicion
+        3. Obtener caracter en nueva posicion
+        Devolver la concatenacion de todos los nuevos caracteres
+        '''
+        mensaje_encriptado=""
+        for caracter in mensaje:
+            if caracter not in self.alfabeto:
+                new_caracter = caracter
+            else:
+                pos = self.alfabeto.index(caracter)
+                new_pos = pos + self.clave
+                new_caracter = self.alfabeto[new_pos]
+            mensaje_encriptado += new_caracter
+        return mensaje_encriptado
--- test_cesar.py
+++ test_cesar.py
@@ -1,10 +1,14 @@
 from cesar import Cesar
-
+import pytest
 def test_create_cesar():
     cesar = Cesar(3)
     assert cesar.clave == 3
 
 def test_clave_must_be_integer():
+    with pytest.raises(AttributeError) as exc:
+        Cesar("algo que no es un entero")
+    assert "debe ser un entero" in str(exc.value)
+
     try:
         Cesar("algo que no es un entero")
         variable_de_comprobacion=0
@@ -12,5 +16,14 @@ def test_clave_must_be_integer():
         variable_de_comprobacion = 1
     assert variable_de_comprobacion == 1
 
+def test_encriptar():
+    cesar = Cesar(3)
+    entrada="HOLA"
+    salida="KRÑD"
+    assert cesar.encriptar(entrada)==salida
 
-    
+def test_encriptar_con_espacio():
+    cesar=Cesar(3)
+    entrada="HOLA PEPE!"
+    salida="KRÑD SHSH!"
+    assert cesar.encriptar(entrada)==salida
