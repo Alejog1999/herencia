a1c87271722018a3b9d21867b09491b99b41121e
Alejandro Gonzalez <jose1999.369@hotmail.com>
2024-02-28 10:58:25 -0500

    test: primeros test de cesar incluido el test  de raise AttributeError

---
+++ test_cesar.py
@@ -0,0 +1,16 @@
+from cesar import Cesar
+
+def test_create_cesar():
+    cesar = Cesar(3)
+    assert cesar.clave == 3
+
+def test_clave_must_be_integer():
+    try:
+        Cesar("algo que no es un entero")
+        variable_de_comprobacion=0
+    except AttributeError:
+        variable_de_comprobacion = 1
+    assert variable_de_comprobacion == 1
+
+
+    
